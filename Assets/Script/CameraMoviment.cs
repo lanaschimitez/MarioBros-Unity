3c10add33535ba493b436f0de54f6b21528ba3e6
lanaschimitez <lana.schimitez@gmail.com>
2020-06-13 23:42:12 -0300

    Level 2

---
+++ Assets/Script/CameraMoviment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraMoviment : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        speed = 4.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, 1f, -10.0f);
+    }
+}
