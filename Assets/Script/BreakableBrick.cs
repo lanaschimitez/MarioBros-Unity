b7d2b48fd6462156d4a9207d25f961103890bd94
lanaschimitez <lana.schimitez@gmail.com>
2020-06-27 18:19:32 -0300

    Atualizacao

---
+++ Assets/Script/BreakableBrick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableBrick : MonoBehaviour
+{
+    public GameObject m_Explosion;
+    private Bounds m_Bounds;
+
+    void Start()
+    {
+        m_Bounds = GetComponent<Collider>().bounds;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("BigPlayer")) return;
+
+        Vector3 impact = collision.contacts[0].point;
+        if (impact.y <= m_Bounds.min.y)
+        {
+            Instantiate(m_Explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
