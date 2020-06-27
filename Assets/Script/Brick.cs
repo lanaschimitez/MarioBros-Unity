b0697b583413c0a29330cc9d8cd16a4ac33edbd5
lanaschimitez <lana.schimitez@gmail.com>
2020-06-27 16:07:01 -0300

    Aula 4

---
+++ Assets/Script/Brick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public float m_BounceTime = 0.3f;
+    public float m_BounceDistance = 0.3f;
+    public AnimationCurve m_Curve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0.5f, 1), new Keyframe(1, 0));
+    private Vector3 m_Source;
+    private bool m_CanBounce;
+    private float m_StartTime;
+    private Bounds m_Bounds;
+
+    void Start()
+    {
+        m_Source = transform.position;
+        m_Bounds = GetComponent<Collider>().bounds;
+    }
+
+    void Update()
+    {
+        if (!m_CanBounce) return;
+        var time = (Time.time - m_StartTime) / m_BounceTime;
+        transform.position = m_Source + Vector3.up * m_BounceDistance * m_Curve.Evaluate(time);
+        m_CanBounce = time < 1.0f;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 impact =  collision.contacts[0].point;
+        if(impact.y <= m_Bounds.min.y)
+        {
+            m_StartTime = Time.time;
+            m_CanBounce = true;
+        }
+    }
+
+}
