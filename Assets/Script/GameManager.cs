0e56c5f73539eb10ba8e234fffeec084c3d4e249
lanaschimitez <lana.schimitez@gmail.com>
2020-08-11 19:23:51 -0300

    Manager

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Android;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int m_life;
+    public bool m_isDead;
+    public LifeUI lifeUI;
     // Start is called before the first frame update
     void Start()
     {
+        lifeUI = GameObject.Find("LifeText").GetComponent<LifeUI>();
         //controlar as moedas
         //controlar as mortes
         //controlar os pontos
@@ -14,8 +19,13 @@ public class GameManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        // verificar a morte do Player
+    }
+
+    public void Dead()
+    {
+        lifeUI.LifeDown();
     }
 }
--- Assets/Script/UIs/LifeUI.cs
+++ Assets/Script/UIs/LifeUI.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
