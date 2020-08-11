411b6d89781c7f3bb0566a719977d4b6a4a1290e
lanaschimitez <lana.schimitez@gmail.com>
2020-08-10 22:51:51 -0300

    Game Manager

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //controlar as moedas
+        //controlar as mortes
+        //controlar os pontos
+        //controlar os niveis
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
